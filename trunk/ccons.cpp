952f88f4d2a4ac8d26ac1f1d0151ebba323a1b09
Alexei.Svitkine <Alexei.Svitkine@69451686-bc0f-11dd-b57b-7192f494c34b>
2009-01-05 18:54:25 +0000

    use google templates to generate the code

--- trunk/ccons.cpp
+++ trunk/ccons.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <google/template.h>
+
+void build_output(const std::string& code, const std::vector<std::string>& headers, std::string* output)
+{
+	google::TemplateDictionary dict("source");
+	dict.SetValue("CODE", code);
+	for (std::vector<std::string>::const_iterator it = headers.begin(); it != headers.end(); ++it)
+		dict.AddSectionDictionary("HEADERS")->SetValue("NAME", *it);
+	google::Template* tpl = google::Template::GetTemplate("skeleton.tpl", google::DO_NOT_STRIP);
+	tpl->Expand(output, &dict);
+}
 
 int main(const int argc, const char **argv)
 {
-	std::cout << "Hello World!" << std::endl;
+	std::string code = "";
+	std::vector<std::string> headers;
+
+	std::string output;
+	build_output(code, headers, &output);
+	std::cout << output;
 	return 0;
 }
