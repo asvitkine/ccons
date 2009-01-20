8ebfd5e26ce5b1789133e7724285b756065fb2b0
Alexei.Svitkine <Alexei.Svitkine@69451686-bc0f-11dd-b57b-7192f494c34b>
2009-01-20 22:11:55 +0000

    process each statement in a separate function, which allows us to maintain the state of execution properly.

--- trunk/Console.h
+++ trunk/Console.h
@@ -2,8 +2,23 @@
 #define CCONS_CONSOLE_H
 
 #include <string>
+#include <vector>
+#include <algorithm>
+
+#include <llvm/ADT/OwningPtr.h>
 
 #include "Parser.h"
+
+namespace llvm {
+	class Linker;
+	class ExecutionEngine;
+} // namespace llvm
+
+namespace clang {
+	class Preprocessor;
+	class Stmt;
+	class Expr;
+} // namespace clang
 
 namespace ccons {
 
@@ -19,9 +34,22 @@ public:
 
 private:
 
-	std::string _includes;
+	enum LineType {
+		StmtLine,
+		DeclLine,
+		PrprLine,
+	};
+
+	std::string genSource();
+	std::string genFunc(std::string line, std::string fname, std::string type);
+	clang::Stmt * lineToStmt(std::string src, std::string line);
+	bool getExprType(const clang::Expr *E, std::string *type);
+
+	llvm::OwningPtr<llvm::Linker> _linker;
+	llvm::OwningPtr<llvm::ExecutionEngine> _engine;
+	llvm::OwningPtr<clang::Preprocessor> _pp;
+	std::vector<std::pair<std::string, LineType> > _lines;
 	std::string _prompt;
-	std::string _body;
 	clang::LangOptions _options;
 	Parser _parser;
 
