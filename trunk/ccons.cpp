10f65b44e4c61bc04978966cb8fa84666ecbd840
Alexei.Svitkine <Alexei.Svitkine@69451686-bc0f-11dd-b57b-7192f494c34b>
2009-02-05 17:09:56 +0000

    add command history and set the default editing mode to emacs

--- trunk/ccons.cpp
+++ trunk/ccons.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <editline/readline.h>
 #include <histedit.h>
 
 #include "Console.h"
@@ -15,19 +16,27 @@ static const char *ccons_prompt(EditLine *e)
 
 int main(const int argc, const char **argv)
 {
+	HistEvent event;
+	History *h = history_init();
+	history(h, &event, H_SETSIZE, INT_MAX);
+
 	EditLine *e = el_init("ccons", stdin, stdout, stderr);
 	el_set(e, EL_PROMPT, ccons_prompt);
+	el_set(e, EL_EDITOR, "emacs");
+	el_set(e, EL_HIST, history, h);
 
 	Console console;
 	cc = &console;
 	const char *line = el_gets(e, NULL);
 	while (line) {
 		console.process(line);
+		history(h, &event, H_ENTER, line);
 		// FIXME: el_push() is broken... the second parameter should be const char *
 		el_push(e, const_cast<char*>(console.input()));
 		line = el_gets(e, NULL);
 	}
 	el_end(e);
+	history_end(h);
 
 	return 0;
 }
