56372509c32c50cdce30f7365aa1ec76f59b56a6
Alexei.Svitkine <Alexei.Svitkine@69451686-bc0f-11dd-b57b-7192f494c34b>
2009-02-12 23:01:48 +0000

    put the edit-line input code into a class behind an interface

--- ccons.cpp
+++ ccons.cpp
@@ -1,55 +1,35 @@
 #include <iostream>
-#include <limits.h>
-#include <histedit.h>
 
 #include <llvm/Support/CommandLine.h>
 #include <llvm/System/Signals.h>
 
 #include "Console.h"
+#include "EditLineReader.h"
 
 using std::string;
 using ccons::Console;
+using ccons::EditLineReader;
 
 static llvm::cl::opt<bool>
 	DebugMode("ccons-debug", llvm::cl::desc("Print debugging information"));
 
-static Console * cc;
-
-static const char *ccons_prompt(EditLine *e)
-{
-	return (cc ? cc->prompt() : "??? ");
-}
 
 int main(const int argc, char **argv)
 {
-	HistEvent event;
-
 	llvm::cl::ParseCommandLineOptions(argc, argv, "ccons Interactive C Console\n");
 	llvm::sys::PrintStackTraceOnErrorSignal();
 
 	if (DebugMode)
 		fprintf(stderr, "NOTE: Debugging information will be displayed.\n");
 
-	History *h = history_init();
-	history(h, &event, H_SETSIZE, INT_MAX);
-
-	EditLine *e = el_init("ccons", stdin, stdout, stderr);
-	el_set(e, EL_PROMPT, ccons_prompt);
-	el_set(e, EL_EDITOR, "emacs");
-	el_set(e, EL_HIST, history, h);
-
 	Console console(DebugMode);
-	cc = &console;
-	const char *line = el_gets(e, NULL);
+
+	EditLineReader reader;
+	const char *line = reader.readLine(console.prompt(), console.input());
 	while (line) {
 		console.process(line);
-		history(h, &event, H_ENTER, line);
-		// FIXME: el_push() is broken... the second parameter should be const char *
-		el_push(e, const_cast<char*>(console.input()));
-		line = el_gets(e, NULL);
+		line = reader.readLine(console.prompt(), console.input());
 	}
-	el_end(e);
-	history_end(h);
 
 	return 0;
 }
