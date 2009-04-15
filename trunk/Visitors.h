d31930bf273510f10e8ce5e32e780c53309a9054
Alexei.Svitkine <Alexei.Svitkine@69451686-bc0f-11dd-b57b-7192f494c34b>
2009-04-15 22:29:05 +0000

    add Visitors files

---
+++ trunk/Visitors.h
@@ -0,0 +1,91 @@
+#ifndef CCONS_VISITORS_H
+#define CCONS_VISITORS_H
+
+#include <string>
+#include <vector>
+
+#include <llvm/ADT/OwningPtr.h>
+
+#include <clang/AST/AST.h>
+#include <clang/AST/ASTConsumer.h>
+#include <clang/AST/DeclGroup.h>
+
+namespace clang {
+	class SourceManager;
+} // namespace clang
+
+namespace ccons {
+
+class StmtFinder : public clang::StmtVisitor<StmtFinder> {
+
+public:
+
+	StmtFinder(unsigned pos, const clang::SourceManager& sm);
+	~StmtFinder();
+
+	void VisitChildren(clang::Stmt *S);
+	void VisitStmt(clang::Stmt *S);
+	clang::Stmt * getStmt() const;
+
+private:
+
+	unsigned _pos;
+	const clang::SourceManager& _sm;
+	clang::Stmt *_S;
+
+};
+
+class StmtSplitter : public clang::StmtVisitor<StmtSplitter> {
+
+public:
+
+	StmtSplitter(const std::string& src,
+	             const clang::SourceManager& sm,
+	             const clang::LangOptions& options,
+							 std::vector<clang::Stmt*> *stmts);
+	~StmtSplitter();
+
+	void VisitChildren(clang::Stmt *S);
+	void VisitStmt(clang::Stmt *S);
+
+private:
+
+	const std::string& _src;
+	const clang::SourceManager& _sm;
+	const clang::LangOptions& _options;
+	std::vector<clang::Stmt*> *_stmts;
+
+};
+
+template <typename T>
+class FunctionBodyConsumer : public clang::ASTConsumer {
+
+private:
+
+	T *_SF;
+	bool _seenFunction;
+
+public:
+
+	explicit FunctionBodyConsumer<T>(T *SF)
+		: _SF(SF), _seenFunction(false) {}
+	~FunctionBodyConsumer<T>() {}
+
+	void HandleTopLevelDecl(clang::DeclGroupRef D) {
+		for (clang::DeclGroupRef::iterator I = D.begin(), E = D.end(); I != E; ++I) {
+			if (clang::FunctionDecl *FD = dyn_cast<clang::FunctionDecl>(*I)) {
+				if (clang::Stmt *S = FD->getBody()) {
+					_SF->VisitChildren(S);
+					_seenFunction = true;
+				}
+			}
+		}
+	}
+
+	bool seenFunction() const { return _seenFunction; }
+
+};
+
+} // namespace ccons
+
+#endif // CCONS_VISITORS_H
