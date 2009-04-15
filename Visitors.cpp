e985ae98f3f45a28df76e545ed5c92d954cf7cc1
Alexei.Svitkine <Alexei.Svitkine@69451686-bc0f-11dd-b57b-7192f494c34b>
2009-04-15 22:29:05 +0000

    add Visitors files

---
+++ Visitors.cpp
@@ -0,0 +1,78 @@
+#include "Visitors.h"
+
+#include <clang/Basic/SourceManager.h>
+#include <clang/Lex/Lexer.h>
+#include <clang/Sema/SemaDiagnostic.h>
+
+using std::string;
+
+namespace ccons {
+
+//
+// StmtFinder
+//
+
+StmtFinder::StmtFinder(unsigned pos, const clang::SourceManager& sm)
+	: _pos(pos), _sm(sm), _S(NULL)
+{
+}
+
+StmtFinder::~StmtFinder()
+{
+}
+
+void StmtFinder::VisitChildren(clang::Stmt *S)
+{
+	for (clang::Stmt::child_iterator I = S->child_begin(), E = S->child_end();
+	     I != E; ++I) {
+		if (*I) {
+			Visit(*I);
+		}
+	}
+}
+
+void StmtFinder::VisitStmt(clang::Stmt *S)
+{
+	clang::SourceLocation Loc = S->getLocStart();
+	if (_sm.getFileOffset(_sm.getInstantiationLoc(Loc)) == _pos) {
+		_S = S;
+	}
+}
+
+clang::Stmt * StmtFinder::getStmt() const
+{
+	return _S;
+}
+
+//
+// StmtSplitter
+//
+
+StmtSplitter::StmtSplitter(const string& src,
+                           const clang::SourceManager& sm,
+                           const clang::LangOptions& options,
+                           std::vector<clang::Stmt*> *stmts)
+		: _src(src), _sm(sm), _options(options), _stmts(stmts)
+{
+}
+
+StmtSplitter::~StmtSplitter()
+{
+}
+
+void StmtSplitter::VisitChildren(clang::Stmt *S)
+{
+	for (clang::Stmt::child_iterator I = S->child_begin(), E = S->child_end();
+	     I != E; ++I) {
+		if (*I) {
+			Visit(*I);
+		}
+	}
+}
+
+void StmtSplitter::VisitStmt(clang::Stmt *S)
+{
+	_stmts->push_back(S);
+}
+
+} // namespace ccons
