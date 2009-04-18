bf452ece0af284245010cab572ff4eca5e7608f8
Alexei.Svitkine <Alexei.Svitkine@69451686-bc0f-11dd-b57b-7192f494c34b>
2009-04-18 07:45:47 +0000

    comments

--- trunk/popen2.h
+++ trunk/popen2.h
@@ -1,6 +1,16 @@
 #ifndef CCONS_POPEN2_H
 #define CCONS_POPEN2_H
 
+//
+// Header file for popen2.c, which declares the popen2() function as
+// a bi-directional variant of popen() function.
+//
+// Part of ccons, the interactive console for the C programming language.
+//
+// Copyright (c) 2009 Alexei Svitkine. This file is distributed under the
+// terms of MIT Open Source License. See file LICENSE for details.
+//
+
 #ifdef __cplusplus
 extern "C" {
 #endif
