1df8e69dd2fac512cf688e1eebf6e09749d4b408
Alexei.Svitkine <Alexei.Svitkine@69451686-bc0f-11dd-b57b-7192f494c34b>
2009-04-20 17:02:02 +0000

    better auto-completion

--- trunk/complete.c
+++ trunk/complete.c
@@ -6,11 +6,37 @@
 #include <dirent.h>
 #include <sys/param.h>
 
+static char next_prefix_char(DIR *d, const char *prefix, unsigned prefix_length)
+{
+	struct dirent *entry;
+	char nextch = '\0';
+	rewinddir(d);
+	for (entry = readdir(d); entry; entry = readdir(d)) {
+		if (!strncmp(entry->d_name, prefix, prefix_length)) {
+			nextch = entry->d_name[prefix_length];
+			break;
+		}
+	}
+	if (nextch != '\0') {
+		rewinddir(d);
+		for (entry = readdir(d); entry; entry = readdir(d)) {
+			if (!strncmp(entry->d_name, prefix, prefix_length)) {
+				if (entry->d_name[prefix_length] != nextch) {
+					nextch = '\0';
+					break;
+				}
+			}
+		}
+	}
+	return nextch;
+}
+
 unsigned complete(const char *path, char *suggest, unsigned suggest_length)
 {
 	const char *dir, *base;
 	char pathcopy[MAXPATHLEN];
 
+	*suggest = 0;
 	strncpy(pathcopy, path, sizeof(pathcopy));
 	pathcopy[sizeof(pathcopy) - 1] = '\0';
 
@@ -23,10 +49,20 @@ unsigned complete(const char *path, char *suggest, unsigned suggest_length)
 			struct dirent *entry;
 			for (entry = readdir(dd); entry; entry = readdir(dd)) {
 				if (!strncmp(entry->d_name, base, len)) {
-					closedir(dd);
-					strncpy(suggest, entry->d_name + len, suggest_length);
+					char prefix[MAXPATHLEN];
+					unsigned prefix_len = len;
+					strncpy(prefix, base, sizeof(prefix) - 1);
+					prefix[sizeof(prefix) - 1] = '\0';
+					while (prefix_len < sizeof(prefix)) {
+						prefix[prefix_len] = next_prefix_char(dd, prefix, prefix_len);
+						if (prefix[prefix_len] == '\0')
+							break;
+						prefix[++prefix_len] = '\0';
+					}
+					strncpy(suggest, prefix + len, suggest_length - 1);
 					suggest[suggest_length - 1] = '\0';
-					return strlen(suggest);
+					closedir(dd);
+					return prefix_len - len;
 				}
 			}
 			closedir(dd);
