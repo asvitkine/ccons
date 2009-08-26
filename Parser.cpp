78c468d572a0939f54d4330a7cfeecb2a92a0b8f
Alexei.Svitkine <Alexei.Svitkine@69451686-bc0f-11dd-b57b-7192f494c34b>
2009-08-26 07:19:49 +0000

    guard against invalid locs

--- Parser.cpp
+++ Parser.cpp
@@ -155,6 +155,8 @@ Parser::InputType Parser::analyzeInput(const string& contextSource,
 				for (clang::DeclGroupRef::iterator I = D.begin(), E = D.end(); I != E; ++I) {
 					if (clang::FunctionDecl *FD = dyn_cast<clang::FunctionDecl>(*I)) {
 						clang::SourceLocation Loc = FD->getTypeSpecStartLoc();
+						if (!Loc.isValid())
+							continue;
 						if (sm->isFromMainFile(Loc)) {
 							unsigned offset = sm->getFileOffset(sm->getInstantiationLoc(Loc));
 							if (offset >= pos) {
